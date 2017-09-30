06ff13b92e1a228aba84e6f447c5d7c7c129c78a
dqf <897432098@qq.com>
2017-09-30 09:38:11 +0800

    增加新的文件

---
+++ QFSafeUtil/NSURL+QFSafeUtil.h
@@ -0,0 +1,13 @@
+//
+//  NSURL+QFSafeUtil.h
+//  TestProject
+//
+//  Created by dqf on 2017/9/30.
+//  Copyright © 2017年 dqfStudio. All rights reserved.
+//
+
+#import "NSObject+QFSwizzleUtil.h"
+
+@interface NSURL (QFSafeUtil)
+
+@end
