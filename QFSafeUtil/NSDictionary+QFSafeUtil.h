1a98e43943b8bef7ed98dc4d8c26c649c5798996
dqf <897432098@qq.com>
2017-09-30 09:10:48 +0800

    增加工程文件

---
+++ QFSafeUtil/NSDictionary+QFSafeUtil.h
@@ -0,0 +1,16 @@
+//
+//  NSDictionary+QFSafeUtil.h
+//  TestProject
+//
+//  Created by dqf on 2017/9/29.
+//  Copyright © 2017年 dqfStudio. All rights reserved.
+//
+
+#import "NSObject+QFSwizzleUtil.h"
+
+@interface NSDictionary (QFSafeUtil)
+@end
+
+@interface NSMutableDictionary (QFSafeUtil)
+@end
+
